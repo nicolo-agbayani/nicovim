eb36b474e878e07bea60baa2b9230591ab22a558
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2024-07-14 18:12:25 -0400

    Disable format on save

--- init.lua
+++ init.lua
@@ -382,20 +382,7 @@ require("lazy").setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones
-        local disable_filetypes = {
-          c = true,
-          cpp = true,
-        }
-
-        return {
-          timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
+      format_on_save = function() end,
       formatters_by_ft = {
         css = { { "prettierd", "prettier" } },
         html = { { "prettierd", "prettier" } },
@@ -502,6 +489,7 @@ require("lazy").setup({
     "rktjmp/lush.nvim",
   },
 
+  -- Custom color scheme
   { dir = "./themes/coffee" },
 
   -- Configure color scheme
