4382dd379d057e255ae8b4e472f63cce294b67bb
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2026-01-06 21:28:28 -0500

    Change go to context shortcut

--- lua/nicovim/lazy/alpha.lua
+++ lua/nicovim/lazy/alpha.lua
@@ -11,9 +11,9 @@ return {
       [[⢿⣿⠀⠀⠀⠀⠀⠙⢿⣷⠀⠀⠀⠀⠀⠙⢿⣷⠀⠙⢿⣿⣿⣿⣿⣷⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⠀⠀⠀⠀⠀⠀⠀⢿⣿⠀⠀⢿⣿⠀⠀⢿⣿]],
     }
     if vim.g.neovide then
-      table.insert(header, [[                                             nicovide 0.7.0]])
+      table.insert(header, [[                                             nicovide 0.7.1]])
     else
-      table.insert(header, [[                                              nicovim 0.7.0]])
+      table.insert(header, [[                                              nicovim 0.7.1]])
     end
     dashboard.section.header.val = header
     alpha.setup(dashboard.config)
--- lua/nicovim/lazy/context.lua
+++ lua/nicovim/lazy/context.lua
@@ -9,8 +9,8 @@ return {
     vim.api.nvim_set_hl(0, "TreesitterContextBottom", { underline = false })
     vim.api.nvim_set_hl(0, "TreesitterContextLineNUmberBottom", { underline = false })
 
-    vim.keymap.set("n", "[c", function()
+    vim.keymap.set("n", "gC", function()
       require("treesitter-context").go_to_context(vim.v.count1)
-    end, { desc = "Go to Context", silent = true })
+    end, { desc = "Go to context", silent = true })
   end,
 }
