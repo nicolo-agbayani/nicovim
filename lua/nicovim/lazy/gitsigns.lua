dd9b5974e7cd3019d7a88b1e974f233760e72749
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2026-03-03 10:38:24 -0500

    Update topdelete character

--- lua/nicovim/lazy/gitsigns.lua
+++ lua/nicovim/lazy/gitsigns.lua
@@ -5,7 +5,7 @@ return {
       add = { text = "+" },
       change = { text = "~" },
       delete = { text = "_" },
-      topdelete = { text = "‾" },
+      topdelete = { text = "-" },
       changedelete = { text = "~" },
     }
   }
