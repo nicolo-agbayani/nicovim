236400f37a47abcbc32e8baef3bdd9f85c261402
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2026-03-03 10:38:55 -0500

    Add lspconfig and autocomplete

---
+++ lua/nicovim/lazy/cmp.lua
@@ -0,0 +1,43 @@
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter",
+  dependencies = {
+    { "L3MON4D3/LuaSnip",
+      build = (function()
+        if vim.fn.has "win32" == 1 or vim.fn.executable "make" == 0 then
+          return
+        end
+        return "make install_jsregexp"
+      end)(),
+    },
+    "saadparwaiz1/cmp_luasnip",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+  },
+  config = function()
+    local cmp = require "cmp"
+    local luasnip = require "luasnip"
+    luasnip.config.setup {}
+
+    cmp.setup {
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      completion = { completeopt = "menu,menuone,noinsert,noselect" },
+      mapping = cmp.mapping.preset.insert {
+        ["<CR>"] = cmp.mapping.confirm { select = false },
+        ["<Tab>"] = cmp.mapping.select_next_item(),
+        ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+        ["<C-Space>"] = cmp.mapping.complete {},
+      },
+      sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+        { name = "path" },
+      },
+    }
+    print(cmp.preselect)
+  end,
+}
