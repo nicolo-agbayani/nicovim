d0efa42537ffe745de6f32370b09079263e661dc
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2026-03-17 15:23:19 -0400

    Overhaul startup version subheader

--- lua/nicovim/lazy/alpha.lua
+++ lua/nicovim/lazy/alpha.lua
@@ -10,11 +10,20 @@ return {
       [[⣿⣿⠀⠀⠀⠙⢿⣷⣄⠀⠀⠀⠀⠙⢿⣷⣄⠙⢿⣷⣄⠀⠀⠀⠀⠙⢿⣷⣄⠀⣠⣶⡄⠉⠀⠀⠀⠙⢿⣷⣿⣿⠀⠀⠀⠙⠿⠃⠀⣿⣿⠀⠀⣿⣿⠀⠀⣿⣿]],
       [[⢿⣿⠀⠀⠀⠀⠀⠙⢿⣷⠀⠀⠀⠀⠀⠙⢿⣷⠀⠙⢿⣿⣿⣿⣿⣷⠀⠙⢿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⠀⠀⠀⠀⠀⠀⠀⢿⣿⠀⠀⢿⣿⠀⠀⢿⣿]],
     }
+
+    local version = "nicovim"
     if vim.g.neovide then
-      table.insert(header, [[                                             nicovide 0.8.0]])
-    else
-      table.insert(header, [[                                              nicovim 0.8.0]])
+      version = "nicovide"
     end
+
+    local hash = vim.fn.system("cd " .. vim.fn.stdpath("config") .. " && git rev-parse HEAD"):gsub("%s+", "")
+    if vim.v.shell_error ~= 0 then
+      hash = "UNKNOWN"
+    end
+
+    version = version .. " // \u{f02a2}" .. string.sub(hash, 1, 7)
+
+    table.insert(header, string.format("%62s", version))
     dashboard.section.header.val = header
     alpha.setup(dashboard.config)
   end
