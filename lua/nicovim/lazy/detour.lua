fbfce807fa366fb48050ec9ca34a20debd136624
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2026-03-23 16:31:56 -0400

    Lazy load detour movement keybinds

--- lua/nicovim/lazy/detour.lua
+++ lua/nicovim/lazy/detour.lua
@@ -1,20 +1,21 @@
 return {
   "carbon-steel/detour.nvim",
+  event = "VimEnter",
   config = function()
     require("detour").setup()
   end,
   keys = {
     { "<C-w><enter>", "<cmd>Detour<cr>", desc = "Detour" },
     { "<C-w>.", "<cmd>DetourCurrentWindow<cr>", desc = "Detour current window" },
-    { "<C-w>j", require("detour.movements").DetourWinCmdJ, mode = { "n", "t" }, desc = "Go to the down window" },
-    { "<C-w><C-j>", require("detour.movements").DetourWinCmdJ, mode = { "n", "t" }, desc = "which_key_ignore" },
-    { "<C-w>h", require("detour.movements").DetourWinCmdH, mode = { "n", "t" }, desc = "Go to the left window" },
-    { "<C-w><C-h>", require("detour.movements").DetourWinCmdH, mode = { "n", "t" }, desc = "which_key_ignore" },
-    { "<C-w>k", require("detour.movements").DetourWinCmdK, mode = { "n", "t" }, desc = "Go to the up window" },
-    { "<C-w><C-k>", require("detour.movements").DetourWinCmdK, mode = { "n", "t" }, desc = "which_key_ignore" },
-    { "<C-w>l", require("detour.movements").DetourWinCmdL, mode = { "n", "t" }, desc = "Go to the right window" },
-    { "<C-w><C-l>", require("detour.movements").DetourWinCmdL, mode = { "n", "t" }, desc = "which_key_ignore" },
-    { "<C-w>w", require("detour.movements").DetourWinCmdW, mode = { "n", "t" }, desc = "Switch windows" },
-    { "<C-w><C-w>", require("detour.movements").DetourWinCmdW, mode = { "n", "t" }, desc = "which_key_ignore" },
+    { "<C-w>j", function() require("detour.movements").DetourWinCmdJ() end, mode = { "n", "t" }, desc = "Go to the down window" },
+    { "<C-w><C-j>", function() require("detour.movements").DetourWinCmdJ() end, mode = { "n", "t" }, desc = "which_key_ignore" },
+    { "<C-w>h", function() require("detour.movements").DetourWinCmdH() end, mode = { "n", "t" }, desc = "Go to the left window" },
+    { "<C-w><C-h>", function() require("detour.movements").DetourWinCmdH() end, mode = { "n", "t" }, desc = "which_key_ignore" },
+    { "<C-w>k", function() require("detour.movements").DetourWinCmdK() end, mode = { "n", "t" }, desc = "Go to the up window" },
+    { "<C-w><C-k>", function() require("detour.movements").DetourWinCmdK() end, mode = { "n", "t" }, desc = "which_key_ignore" },
+    { "<C-w>l", function() require("detour.movements").DetourWinCmdL() end, mode = { "n", "t" }, desc = "Go to the right window" },
+    { "<C-w><C-l>", function() require("detour.movements").DetourWinCmdL() end, mode = { "n", "t" }, desc = "which_key_ignore" },
+    { "<C-w>w", function() require("detour.movements").DetourWinCmdW() end, mode = { "n", "t" }, desc = "Switch windows" },
+    { "<C-w><C-w>", function() require("detour.movements").DetourWinCmdW() end, mode = { "n", "t" }, desc = "which_key_ignore" },
   },
 }
