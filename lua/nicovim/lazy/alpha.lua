d6c69d083a1b243446124f5060cabb0f7585310b
Nicolo Agbayani <37029146+nicolo-agbayani@users.noreply.github.com>
2026-03-23 16:31:37 -0400

    Fix formatting

--- lua/nicovim/lazy/alpha.lua
+++ lua/nicovim/lazy/alpha.lua
@@ -1,6 +1,6 @@
 return {
   "goolord/alpha-nvim",
-  config = function ()
+  config = function()
     local alpha = require("alpha")
     local dashboard = require("alpha.themes.dashboard")
     local header = {
--- lua/nicovim/lazy/noice.lua
+++ lua/nicovim/lazy/noice.lua
@@ -4,7 +4,7 @@ return {
   dependencies = {
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
+  config = function()
     require("noice").setup()
   end,
   keys = {
